c0641708cba9db5c3e0326258342bf6100cc908c
lgili <luizcarlosgili@gmail.com>
2022-01-05 10:09:40 -0300

    add clk properties

--- axis_ad7276/tcl/axis_ad7276.tcl
+++ axis_ad7276/tcl/axis_ad7276.tcl
@@ -9,8 +9,10 @@ set_property source_mgmt_mode All ${ip_project}
 # Read source files from hdl directory
 set v_src_files [glob ./hdl/*.v]
 set sv_src_files [glob ./hdl/*.sv]
+set mem_src_files [glob ./hdl/*.mem]
 read_verilog ${v_src_files}
 read_verilog -sv ${sv_src_files}
+read_mem ${mem_src_files}
 update_compile_order -fileset sources_1
 
 # Package project and set properties
@@ -22,7 +24,7 @@ set_property SUPPORTED_FAMILIES ${family_lifecycle} ${ip_core}
 # Associate AXI/AXIS interfaces and reset with clock
 set aclk_intf [ipx::get_bus_interfaces ACLK -of_objects ${ip_core}]
 set aclk_assoc_intf [ipx::add_bus_parameter ASSOCIATED_BUSIF $aclk_intf]
-set_property value M_AXIS:S_AXIS:S_AXI $aclk_assoc_intf
+set_property value s00_axi:m00_axis:m01_axis:m02_axis:m03_axis:m04_axis:m05_axis $aclk_assoc_intf
 set aclk_assoc_reset [ipx::add_bus_parameter ASSOCIATED_RESET $aclk_intf]
 set_property value ARESETN $aclk_assoc_reset
 
